fd1c1935357d0ba10d8933f5cd9bc49a4c83b7ad
dennis29 <manullang_d@yahoo.com>
2019-10-09 08:56:01 +0700

    etherus

--- public/lib/function_Guest.js
+++ public/lib/function_Guest.js
@@ -1,13 +1,33 @@
-function save_body(zonename){
-	alert('Save Zone');
-	localStorage[zonename] = $('#table_1').html();
-}
-
 function load_body(zonename){
 	if(localStorage[zonename])
-	{	
-		$('#table_1').html(localStorage[zonename]);
-	}	
+	{
+		
+	   $('#table_1').html(localStorage[zonename]);	
+	}
+
 }
 
 load_body(zone);
+
+
+function change_image(zone,arr=[]){				   
+		          for (i = 0; i < arr.length; i++) {
+					if(document.getElementById(arr[i]).className  == "green")
+					{
+							document.getElementById(arr[i]).src = "/"+zone+"/images/"+arr[i]+"_red.jpg"; 
+							document.getElementById(arr[i]).className  = "red";
+					}
+					else
+					{
+							document.getElementById(arr[i]).src = "/"+zone+"/images/"+arr[i]+"_green.jpg"; 
+							document.getElementById(arr[i]).className  = "green";
+					}
+							var container = document.querySelector("#"+arr[i]);
+				  } 
+        
+}
+
+
+
+
+	
