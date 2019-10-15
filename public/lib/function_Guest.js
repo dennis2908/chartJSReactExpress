b7132010dfbae3b2a7c2946709b418b64ad4ee81
dennis29 <manullang_d@yahoo.com>
2019-10-15 18:01:54 +0700

    etherus

--- public/lib/function_Guest.js
+++ public/lib/function_Guest.js
@@ -2,28 +2,107 @@ function load_body(zone_id){
 	  
 	$.ajax({
 	method: "GET",	
-    url:"crud/get_by_id", 
+    url:"/crud/get_by_id", 
     data: { id: zone_id},
     success:function(data) {
-		$('#table_1').html(data);
+		if(data)
+		{
+			$('#table_1').html(data);
+		}
+			
 	  }
+	}).done(function( data ) {
+		      $('img,div').each(function () {
+
+				var arrx = ['green','blue','red','green-dragging','red-dragging','blue-dragging','green ui-draggable-dragging','red ui-draggable-dragging'
+				,'red ui-draggable-dragging'];
+				var arry = ['border_red rectangle','border_green rectangle','border_blue rectangle'];
+				var arrz = ['merge'];
+				var i = $(this).attr('class');
+				
+			    if(i)
+				{
+ 				  if(i.includes('ui-draggable'))
+				  {
+					$(this).attr('class',$(this).attr('class').replace(' ui-draggable',''));
+				  }
+				}
+				if(i)
+				{
+				  if(i.includes('-dragging'))
+				  {
+					$(this).attr('class',$(this).attr('class').replace('-dragging',''));
+				  }
+				}
+				
+				var i = $(this).attr('class');
+				
+				if(arrx.includes(i))
+				{
+					$(this).draggable();
+					$( this ).css('z-index','1');
+				
+					$(this).mouseover(function(){
+						$( this ).css('z-index','11');
+					});
+				}
+				
+				if(arrz.includes(i))
+				{
+					$(this).draggable();
+				}
+				
+				if(arry.includes(i))
+				{
+					$(this).click(function() {
+						if($(this).attr('class')=="border_blue rectangle ui-draggable")
+						{
+									
+						   $(this).attr('class','border_red rectangle ui-draggable');
+										  
+						}
+						else if($(this).attr('class')=="border_green rectangle ui-draggable")
+						{
+									
+						   $(this).attr('class','border_blue rectangle ui-draggable');
+										  
+						}
+						else{
+						  
+						   $(this).attr('class','border_green rectangle ui-draggable');
+						}		
+						
+					});
+					$(this).draggable();
+				}
+			
+			});
+
 	});
 }
 
-function save_body(zone_id){
-	$.post("/crud/save",
-	{
-		body: $('#table_1').html(),
-		id: zone_id,
-	},function(data, status){
-		alert("Zone " + zone_id + " saved");
-	});
-}
-
-function reset_index(){					
-		 $("img").css('z-index','1');
-		 $("div").css('z-index','1');
-		 alert('Z index Reset');
+function save_body(zone_id,zone_to){
+	
+	$.ajax({
+        url: "/crud/save",
+        type: "post",
+        data: {	
+			body: $('#table_1').html(),
+			id: zone_id
+		} ,
+        success: function (response) {
+           if(zone_to)
+			{
+				
+				 var loc = window.location;
+				 
+				 window.location.href = "/"+zone_to+"/";
+			}
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+           console.log(textStatus, errorThrown);
+        }
+    });
 }
 	
 load_body(zone);
@@ -46,7 +125,14 @@ function change_image(zone,arr=[]){
         
 }
 
+function reset_index(){					
+		 $("img").css('z-index','1');
+		 $("div").css('z-index','1');
+		 alert('Z index Reset');
+}	
 
-
+function redirect_to(zoneto){
+	  save_body(zone,zoneto);
+} 		
 
 	
