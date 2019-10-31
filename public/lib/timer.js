3c475859d4aba1bac3c406e038a917320b40da32
dennis29 <manullang_d@yahoo.com>
2019-10-31 17:11:00 +0700

    tqm

--- public/lib/timer.js
+++ public/lib/timer.js
@@ -8,30 +8,8 @@ function timerW()
     localStorage['item']--;
     console.log(localStorage['item']);
     if (localStorage['item'] == 0) {
-		localStorage['position'] = $(window).scrollTop();
-	//	localStorage.clear();
-	///	localStorage.removeItem('item');
 		clearInterval(interval_to_save);
 		window.location.reload(); 
     }
 }, 1000);
-}
-
-function timerS(timer_to)
-{
-	/*
-	localStorage['count_to_save'] = timer_to;
-	var interval_to_reload = setInterval(function () {
-	console.log(localStorage['count_to_save']);
-    localStorage['count_to_save']--;
-    if (localStorage['count_to_save'] == 0) {
-	//	localStorage.clear();
-	///	localStorage.removeItem('item');
-	    save_body();
-		clearInterval(interval_to_reload);
-
-    }
-}, 1000);
-
-*/
 }
