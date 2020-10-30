cad0fc1b40706ee8d823bcea4732298bcb03d8cf
dennis M <manullang_d@yahoo.com>
2020-10-30 15:50:53 +0700

    dmdmdm

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
-var pg = require('pg');
+var pg = require('pg')
 var config = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'penjualan',
-  password: '12345',
+  user: 'wkijhdswmbdagm',
+  host: 'ec2-52-203-165-126.compute-1.amazonaws.com',
+  database: 'd55elu3a9h380s',
+  password: '3ac0c112c759a5e8bc233b5600f8ffce292df608a4e7b34822f5a46fb13ceddd',
   port: 5432,
   max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 
+  idleTimeoutMillis: 30000,
+  rejectUnauthorized: false  
 }
 var pool = new pg.Pool(config);
 module.exports = pool;
