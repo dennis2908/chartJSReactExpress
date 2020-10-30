870c999258c83c2e11f92159c3c7b32bd3556ed0
dennis M <manullang_d@yahoo.com>
2020-10-30 14:21:50 +0700

    dmdmdm

--- database.js
+++ database.js
@@ -1,14 +1,13 @@
-var pg = require('pg');
-pg.defaults.ssl = true;
+var pg = require('pg')
 var config = {
   user: 'wkijhdswmbdagm',
   host: 'ec2-52-203-165-126.compute-1.amazonaws.com',
   database: 'd55elu3a9h380s',
   password: '3ac0c112c759a5e8bc233b5600f8ffce292df608a4e7b34822f5a46fb13ceddd',
   port: 5432,
-  max: 0, // max number of clients in the pool
-  idleTimeoutMillis: 0,
-  ssl: { rejectUnauthorized: false }  
+  max: 10000, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
+  ssl: { rejectUnauthorized: true }   
 }
 var pool = new pg.Pool(config);
 module.exports = pool;
