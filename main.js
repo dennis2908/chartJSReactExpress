aa9baef1b5c899a89edb619fb8f1e09eca1177a0
dennis M <manullang_d@yahoo.com>
2020-10-30 15:29:52 +0700

    dmdmdm

--- main.js
+++ main.js
@@ -93,8 +93,8 @@ function isAuthenticatedAllZone(req, res, next) {
 	}
 }
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('server running on port 3000', '');
+app.listen(process.env.PORT || 8000, function() {
+    console.log('server running on port 8000', '');
 });
 
 	
